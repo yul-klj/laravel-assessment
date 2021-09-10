c5640ebde501b0f6d21cfe8233e9eed43f8588ce
Yul <yul_klj@hotmail.com>
2021-09-11 01:52:22 +0800

    Commit basics CRUD and search controller function with api routes

---
+++ laravel/app/Http/Controllers/BookController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\BookService;
+use App\Http\Requests\BookRequest;
+use App\Http\Requests\BookUrlRequest;
+use App\Http\Requests\BookGetAllRequest;
+use App\Http\Requests\BookSearchRequest;
+use App\Http\Requests\BookUpdateRequest;
+
+/**
+ * Class BookController
+ *
+ * @package App\Http\Controllers
+ * @author  Yul <yul_klj@hotmail.com>
+ */
+class BookController extends Controller
+{
+    /**
+     * Create Book
+     *
+     * @param BookRequest $request Book request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function create(BookRequest $request)
+    {
+        $inputs = $request->only(['title', 'author']);
+        $book = app(BookService::class)->create($inputs);
+
+        return $this->respondAccomplished('Book created', $book);
+    }
+
+    /**
+     * Get all book
+     *
+     * @param BookGetAllRequest $request Book request
+     * @return \Illuminate\Http\Response
+     */
+    public function all(BookGetAllRequest $request)
+    {
+        $orderField = $request->input('order_field', 'id');
+        $orderClause = $request->input('order_clause', 'asc');
+        $books = app(BookService::class)->allBook($orderField, $orderClause);
+        return $this->respondAccomplished('Books retrived', $books);
+    }
+
+    /**
+     * Get Single Book Detail
+     *
+     * @param int            $id      Book id
+     * @param BookUrlRequest $request Book request
+     * @return \Illuminate\Http\Response
+     */
+    public function get(int $id, BookUrlRequest $request)
+    {
+        $book = app(BookService::class)->getBookDetail($id);
+
+        if (empty($book)) {
+            return $this->errorNotFound();
+        } else {
+            return $this->respondAccomplished('Book found', $book);
+        }
+    }
+
+    /**
+     * Update Single Book Detail
+     *
+     * @param int               $id      Book id to update
+     * @param BookUpdateRequest $request Book request
+     *
+     * @return array|\Illuminate\Http\Response
+     */
+    public function update(int $id, BookUpdateRequest $request)
+    {
+        $bookData = $request->only(['title', 'author']);
+        $book = app(BookService::class)->update($id, $bookData);
+        if (empty($book)) {
+            return $this->errorNotFound();
+        }
+
+        return $this->respondAccomplished('Book updated', $book);
+    }
+
+    /**
+     * Delete Book
+     *
+     * @param int            $id      Book id to delete
+     * @param BookUrlRequest $request Book request
+     * @return array|\Illuminate\Http\Response
+     */
+    public function delete(int $id, BookUrlRequest $request)
+    {
+        $success = app(BookService::class)->delete($id);
+        if (! $success) {
+            return $this->errorNotFound();
+        }
+        return $this->respondAccomplished();
+    }
+
+    /**
+     * Search Book
+     *
+     * @param BookUrlRequest $request Book request
+     * @return array|\Illuminate\Http\Response
+     */
+    public function search(BookSearchRequest $request)
+    {
+        $keyword = $request->input('keyword', '');
+        $orderField = $request->input('order_field', 'id');
+        $orderClause = $request->input('order_clause', 'asc');
+        $books = app(BookService::class)->search(
+            $keyword,
+            $orderField,
+            $orderClause
+        );
+
+        return $this->respondAccomplished('Searched book result', $books);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('/books/search', 'App\Http\Controllers\BookController@search');
+    Route::get('/books', 'App\Http\Controllers\BookController@all');
+    Route::post('/book', 'App\Http\Controllers\BookController@create');
+    Route::get('/book/{id}', 'App\Http\Controllers\BookController@get');
+    Route::put('/book/{id}', 'App\Http\Controllers\BookController@update');
+    Route::delete('/book/{id}', 'App\Http\Controllers\BookController@delete');
+});
