24b5b4304b5414695ce436797605b966d3debb52
Yul <yul_klj@hotmail.com>
2021-09-11 13:32:16 +0800

    Remove unuse route, clean up and grouped route in a proper manner

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,15 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['prefix' => 'v1'], function () {
-    Route::get('/books/search', 'App\Http\Controllers\BookController@search');
-    Route::get('/books', 'App\Http\Controllers\BookController@all');
-    Route::post('/book', 'App\Http\Controllers\BookController@create');
-    Route::get('/book/{id}', 'App\Http\Controllers\BookController@get');
-    Route::put('/book/{id}', 'App\Http\Controllers\BookController@update');
-    Route::delete('/book/{id}', 'App\Http\Controllers\BookController@delete');
+    Route::group(['prefix' => 'book'], function () {
+        Route::post('/', 'App\Http\Controllers\BookController@create');
+        Route::get('/{id}', 'App\Http\Controllers\BookController@get');
+        Route::put('/{id}', 'App\Http\Controllers\BookController@update');
+        Route::delete('/{id}', 'App\Http\Controllers\BookController@delete');
+    });
+
+    Route::group(['prefix' => 'books'], function () {
+        Route::get('/search', 'App\Http\Controllers\BookController@search');
+        Route::get('/', 'App\Http\Controllers\BookController@all');
+    });
+
+    Route::group(['prefix' => 'export'], function () {
+        Route::get('/{id}', 'App\Http\Controllers\ExportController@retrieveExport');
+        Route::post('/', 'App\Http\Controllers\ExportController@initializeExport');
+    });
 });
