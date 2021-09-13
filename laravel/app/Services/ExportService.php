c6e47636d6a497e460540844c868e93fdee7827e
Yul <yul_klj@hotmail.com>
2021-09-13 16:45:45 +0800

    Export code reafactor, adds validation and function call rename

--- laravel/app/Services/ExportService.php
+++ laravel/app/Services/ExportService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Exception;
 use Spatie\ArrayToXml\ArrayToXml;
 use App\Repositories\BookRepository;
 use App\Repositories\ExportRepository;
@@ -40,7 +41,7 @@ class ExportService
      */
     public function generateExportEntry(array $data)
     {
-        $hasField = $this->bookRepository->validateDbField($data['field']);
+        $hasField = $this->bookRepository->validDatabaseField($data['field']);
 
         if (! $hasField) {
             $fields = $this->bookRepository->getAllRelatedDbField();
@@ -95,13 +96,10 @@ class ExportService
      */
     public function csvExport(int $id)
     {
-        $exportDetail = $this->getExportDetail($id);
-        if (empty($exportDetail)) {
-            return;
-        }
-        $fields = explode(',', $exportDetail['fields']);
+        [$exportDetail, $books] =
+            $this->getExportArrayAndBooksObjectByExportId($id);
 
-        $books = $this->bookRepository->getAllByFields($fields);
+        $fields = explode(',', $exportDetail['fields']);
 
         $time = time();
         $fileName = "csv-export-$time.csv";
@@ -132,13 +130,8 @@ class ExportService
      */
     public function xmlExport(int $id)
     {
-        $exportDetail = $this->getExportDetail($id);
-        if (empty($exportDetail)) {
-            return;
-        }
-        $fields = explode(',', $exportDetail['fields']);
-
-        $books = $this->bookRepository->getAllByFields($fields);
+        [$exportDetail, $books] =
+            $this->getExportArrayAndBooksObjectByExportId($id);
         $result = ArrayToXml::convert(['book' => $books->toArray()], 'books');
         $time = time();
         $fileName = "xml-export-$time.xml";
@@ -146,5 +139,54 @@ class ExportService
 
         $exportDetail['location'] = asset('storage/' . $fileName);
         $this->updateExportDetail($exportDetail['id'], $exportDetail);
+    }
+
+    /**
+     * This function is for:-
+     * - get export detail
+     * - call validate function
+     * - get all book object
+     *
+     * @param int $id Export id to retrieve
+     * @return [array, Book] [$exportDetail, $bookObjects] export detail array and book data object
+     * @throws \Exception
+     */
+    private function getExportArrayAndBooksObjectByExportId(int $id)
+    {
+        $exportDetail = $this->getExportDetail($id);
+        $this->validateExportEntryData($exportDetail);
+
+        $fields = explode(',', $exportDetail['fields']);
+        $bookObjects = $this->bookRepository->getAllByFields($fields);
+
+        return [$exportDetail, $bookObjects];
+    }
+
+    /**
+     * This function is for:-
+     * - the usage of validate export exists
+     * - the usage of validate export type
+     * - the usage of validate export fields
+     *
+     * @param array $exportDetail Export detail data passed in for validation
+     * @throws \Exception
+     */
+    private function validateExportEntryData(array $exportDetail)
+    {
+        if (empty($exportDetail)) {
+            throw new Exception('No export to be process.');
+        }
+
+        if (! $this->exportRepository->validExportType($exportDetail['type'])) {
+            throw new Exception('Unknown type being selected.');
+        }
+
+        $fields = explode(',', $exportDetail['fields']);
+        foreach ($fields as $field) {
+            $validColumn = $this->bookRepository->validDatabaseField($field);
+            if (! $validColumn) {
+                throw new Exception('Unknown fields being selected.');
+            }
+        }
     }
 }
