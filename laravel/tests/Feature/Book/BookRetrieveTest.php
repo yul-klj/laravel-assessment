202724b0cda2fd8a40c9d736eab2b6f5726ec8c1
Yul <yul_klj@hotmail.com>
2021-09-13 16:56:10 +0800

    Fix retrieve book feature test failed cause by api enhancement

--- laravel/tests/Feature/Book/BookRetrieveTest.php
+++ laravel/tests/Feature/Book/BookRetrieveTest.php
@@ -6,6 +6,12 @@ use Tests\TestCase;
 use App\Models\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+/**
+ * Feature Test for Retrieve book
+ *
+ * @package Tests\Feature
+ * @author Yul <yul_klj@hotmail.com>
+ */
 class BookRetrieveTest extends TestCase
 {
     use RefreshDatabase;
@@ -31,7 +37,8 @@ class BookRetrieveTest extends TestCase
     }
 
     /**
-     * Test Books retrival success
+     * Test Books retrival success, this api checks without paginations
+     * It will returns all the data in Database
      *
      * @return void
      */
@@ -41,18 +48,19 @@ class BookRetrieveTest extends TestCase
 
         $response
             ->assertStatus(200)
-            ->assertJsonPath('content.data.data.0.id', 1)
-            ->assertJsonPath('content.data.data.0.author', 'Yul');
+            ->assertJsonPath('content.data.0.id', 1)
+            ->assertJsonPath('content.data.0.author', 'Yul');
     }
 
     /**
      * Test Books search success
+     * Expect search result, comparing returned value
      *
      * @return void
      */
     public function testSearchBookApiRequestSuccess()
     {
-        $response = $this->get('/api/v1/books?keyword=Yul');
+        $response = $this->get('/api/v1/books/search?keyword=Yul');
 
         $response
             ->assertStatus(200)
@@ -62,6 +70,7 @@ class BookRetrieveTest extends TestCase
 
     /**
      * Test Books search no result success
+     * Expecting empty array returned
      *
      * @return void
      */
